367fbb05b2934061560b6b430731aecebc5364ad
stas-bukovskiy <stbukovskiy12@gmail.com>
2023-11-07 23:43:11 +0200

    feat: add unit testing

--- src/lib.rs
+++ src/lib.rs
@@ -1,36 +1,36 @@
 extern crate pest;
 #[macro_use]
 extern crate pest_derive;
-extern crate html_escape;
 
 use pest::Parser;
-use html_escape::encode_text;
 
 #[derive(Parser)]
 #[grammar = "markdown.pest"]
-pub struct MarkdownParser;
+struct MarkdownParser;
 
-pub fn parse(input: &str) -> String {
-    let pairs = MarkdownParser::parse(Rule::document, input).expect("Failed to parse input.");
+pub fn parse_markdown(markdown_text: &str) -> String {
+    let pairs = MarkdownParser::parse(Rule::markdown, markdown_text).unwrap_or_else(|e| panic!("{}", e));
 
-    let mut output = String::new();
+    let mut html = String::new();
 
     for pair in pairs {
-        match pair.as_rule() {
-            Rule::text => {
-                output += &encode_text(pair.as_str());
-            }
-            Rule::italic => {
-                output += &format!("<em>{}</em>", pair.as_str());
-            }
-            Rule::bold => {
-                output += &format!("<strong>{}</strong>", pair.as_str());
-            }
-            _ => {}
+        for inner_pair in pair.into_inner() {
+            match inner_pair.as_rule() {
+                Rule::h1 => {
+                    html.push_str(&format!("<h1>{}</h1>", inner_pair.as_str().replace("# ", "")));
+                }
+                Rule::h2 => {
+                    html.push_str(&format!("<h2>{}</h2>",  inner_pair.as_str().replace("## ", "")));
+                }
+                Rule::paragraph => {
+                    html.push_str(&format!("<p>{}</p>",  inner_pair.as_str()));
+                }
+                _ => {}
+            };
         }
     }
 
-    output
+    html
 }
 
 
@@ -38,9 +38,31 @@ pub fn parse(input: &str) -> String {
 mod tests {
     use super::*;
 
+    // #[test]
+    // fn it_works() {
+    //     let markdown_text = "# Heading 1\n## Heading 2\n\nThis is a paragraph.";
+    //     let html = parse_markdown(&markdown_text);
+    //     println!("{}", html);
+    // }
+
+
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn test_valid_header() {
+        let markdown_text = "# Header 1";
+        let html = parse_markdown(markdown_text);
+        assert_eq!(html, "<h1>Header 1</h1>");
+    }
+
+    #[test]
+    fn test_valid_paragraph() {
+        let markdown_text = "This is a paragraph";
+        let html = parse_markdown(markdown_text);
+        assert_eq!(html, "<p>This is a paragraph</p>");
+    }
+
+    #[test]
+    fn test_invalid_header() {
+        let markdown_text = "#Invalid Header";
+        assert_eq!(markdown_text, "#Invalid Header");
     }
 }
